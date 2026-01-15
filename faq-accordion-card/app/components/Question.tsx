b9d72e74e8fb8f62374bd2f03554fc8e38d7b01c
tsiklaurii <tsiklaurinino321@gmail.com>
2026-01-16 00:31:03 +0400

    up

--- faq-accordion-card/app/components/Question.tsx
+++ faq-accordion-card/app/components/Question.tsx
@@ -1,9 +1,29 @@
 'use client'
+import Image from "next/image";
+import { Kumbh_Sans } from 'next/font/google';
+import { useState } from "react";
 
-const Question = () => {
+const kumbSansFont = Kumbh_Sans({
+    subsets: ["latin"],
+})
+
+type QuestionTypes = {
+    question: string,
+    answer: string
+}
+
+const Question = ({ question, answer }: QuestionTypes) => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div>
-            <button onClick={() => console.log("smths")}>click</button>
+        <div className="border-b border-b-[#E8E8EA] p-[18px_0px_18px_0px] w-87.5 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+            <div className="flex place-content-between pr-4">
+                <h1 className={`text-[14px] ${kumbSansFont.className} ${isOpen ? 'font-bold' : 'text-[#4B4C5F]'} transition-all duration-300`}>{question}</h1>
+                <Image src="/arrow.svg" alt="file" width={8} height={4} className={`cursor-pointer ${isOpen ? 'rotate-180' : ''}`} />
+            </div>
+            <div className={`overflow-hidden transition-all duration-300 ${isOpen ? 'max-h-25 opacity-100' : 'max-h-0 opacity-0'}`}>
+                <p className='text-[12px] text-[#787887] leading-4.5 mt-2.75 w-79.75'>{answer}</p>
+            </div>
         </div>
     )
 }
--- faq-accordion-card/app/faq/page.tsx
+++ faq-accordion-card/app/faq/page.tsx
@@ -1,18 +1,41 @@
 import Question from '../components/Question'
 import Image from "next/image";
+import { Kumbh_Sans } from 'next/font/google';
+
+const kumbSansFont = Kumbh_Sans({
+    subsets: ["latin"],
+    weight: "700",
+})
 
 const page = () => {
     return (
         <div className='h-screen bg-linear-to-t from-[#6463E7] to-[#B068E9] flex justify-center items-center'>
             <div className='w-230 h-127.25 rounded-[23px] bg-[#FFFFFF] drop-shadow-[0_50px_50px_#35127A7F] flex'>
                 <div className='w-[50%] relative overflow-hidden'>
-                    <Image src="/faq-image.svg" alt="file" width={472} height={359} className='absolute z-2 top-[70px] left-[-40px]' />
-                    <Image src="/faq-shadow.svg" alt="file" width={964} height={568} className='absolute z-1 top-20.5 -left-10' />
+                    <Image src="/line.svg" alt="file" width={500} height={400} className='absolute z-2 left-[-40px]' />
+                    <Image src="/line.svg" alt="file" width={500} height={400} className='absolute z-2 -top-20 -left-10' />
+                    <Image src="/illustration.svg" alt="file" width={472} height={359} className='absolute z-2 top-17.5 -left-10' />
+                    <Image src="/shadow.svg" alt="file" width={964} height={568} className='absolute z-1 top-20.5 -left-10' />
                 </div>
-                <Image src="/faq-box.svg" alt="file" width={200} height={106} className='absolute z-3 top-55 -left-24.25' />
+                <Image src="/box.svg" alt="file" width={200} height={106} className='absolute z-3 top-55 -left-24.25' />
 
                 <div className='border border-amber-50 w-[50%]'>
-                    <Question />
+                    <h1 className={`text-[32px] text-[#1E1F36] p-[65px_0px_25px_0px] ${kumbSansFont.className}`}>FAQ</h1>
+                    <Question
+                        question='How many team members can I invite?'
+                        answer='You can invite up to 5 team members.' />
+                    <Question
+                        question='What is the maximum file upload size?'
+                        answer='No more than 2GB. All files in your account must fit your allotted storage space.' />
+                    <Question
+                        question='How do I reset my password?'
+                        answer='Click "forgot password" on the page and follow the instructions.' />
+                    <Question
+                        question='Can I cancel my subscription?'
+                        answer='Yes.' />
+                    <Question
+                        question='Do you provide additional support?'
+                        answer='Yes.' />
                 </div>
             </div>
         </div>
